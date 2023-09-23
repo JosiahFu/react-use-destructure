86cd13b232ce93e30c4edff8b101c1127b14f850
JosiahFu <josiahfu@gmail.com>
2023-09-23 13:28:22 -0700

    Improve type inference

--- src/usePropState.ts
+++ src/usePropState.ts
@@ -1,15 +1,17 @@
 import { useCallback, useRef } from 'react';
 
-function usePropState<T extends object, K extends keyof T>(object: T, setObject: (value: T) => void, key: K): [T[K], (value: T[K]) => void] {
-    type P = T[K];
+function usePropState<T, K extends keyof T>(object: T, setObject: (value: T) => void, key: K) {
+
+    type Prop = T extends { [Key in K]: infer U } ? U : never;
+
     const objectRef = useRef(object);
     objectRef.current = object;
 
-    const setState = useCallback((value: P) => {
+    const setState = useCallback((value: Prop) => {
         setObject({ ...objectRef.current, [key]: value });
     }, [key, setObject]);
 
-    return [object[key], setState];
+    return [object[key], setState] as [Prop, (value: Prop) => void];
 }
 
 export { usePropState };
