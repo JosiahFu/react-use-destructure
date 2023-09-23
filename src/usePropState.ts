afe9fe1343effaf93447a6c180cef4eb858acaca
JosiahFu <josiahfu@gmail.com>
2023-09-23 13:36:26 -0700

    Revert previous change, it did not help

--- src/usePropState.ts
+++ src/usePropState.ts
@@ -1,17 +1,14 @@
 import { useCallback, useRef } from 'react';
 
-function usePropState<T, K extends keyof T>(object: T, setObject: (value: T) => void, key: K) {
-
-    type Prop = T extends { [Key in K]: infer U } ? U : never;
-
+function usePropState<T extends object, K extends keyof T>(object: T, setObject: (value: T) => void, key: K): [T[K], (value: T[K]) => void] {
     const objectRef = useRef(object);
     objectRef.current = object;
 
-    const setState = useCallback((value: Prop) => {
+    const setState = useCallback((value: T[K]) => {
         setObject({ ...objectRef.current, [key]: value });
     }, [key, setObject]);
 
-    return [object[key], setState] as [Prop, (value: Prop) => void];
+    return [object[key], setState];
 }
 
 export { usePropState };
